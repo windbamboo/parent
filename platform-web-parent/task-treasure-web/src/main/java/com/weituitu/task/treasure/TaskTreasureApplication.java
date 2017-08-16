93a416449d9d5c7d6f7401329d16e04d29348a48
windbamboo <windbamboo@163.com>
2017-08-16 23:56:41 +0800

    init project

---
+++ platform-web-parent/task-treasure-web/src/main/java/com/weituitu/task/treasure/TaskTreasureApplication.java
@@ -0,0 +1,18 @@
+package com.weituitu.task.treasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @描述:任务宝项目启动类
+ * @作者:liuguozhu
+ * @创建:2017/8/16-下午10:45
+ * @版本:v1.0
+ */
+@SpringBootApplication
+public class TaskTreasureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTreasureApplication.class, args);
+    }
+}
